ec5469328d25617e09952438fa473f08c9039e1f
Elvis Perdomo <Perdomoelvis@hotmail.com>
2022-06-07 16:28:54 -0400

    added REST request features

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-
+app.use(express.json())
 
 let notes = [
   {
@@ -52,6 +52,46 @@ app.get('/api/notes/:id', (request, response) => {
       
         response.status(204).end()
       })
+
+      app.post('/api/notes', (request, response) => {
+        const note = request.body
+        console.log(note)
+        response.json(note)
+      })
+
+      const generateId = () => {
+        const maxId = notes.length > 0
+          ? Math.max(...notes.map(n => n.id))
+          : 0
+        return maxId + 1
+      }
+      
+      app.post('/api/notes', (request, response) => {
+        const body = request.body
+      
+        if (!body.content) {
+          return response.status(400).json({ 
+            error: 'content missing' 
+          })
+        }
+      
+        const note = {
+          content: body.content,
+          important: body.important || false,
+          date: new Date(),
+          id: generateId(),
+        }
+      
+        notes = notes.concat(note)
+      
+        response.json(note)
+      })
+
+      const unknownEndpoint = (request, response) => {
+        response.status(404).send({ error: 'unknown endpoint' })
+      }
+      
+      app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
